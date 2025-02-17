373735b62bf18be6622f843b5bf831a63d798796
dnkk21 <danikkacarreon@gmail.com>
2025-02-17 16:38:42 +0800

    update choose docs

--- AGQ/agq_login.php
+++ AGQ/agq_login.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,14 +11,14 @@
     <!-- Font Link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">    
+    
     <!-- Local CSS -->
-    <link rel="stylesheet" type="text/css" href="../css/Login.css">
+    <link rel = "stylesheet" type="text/css" href="agq.css">
 
 </head>
-<link rel="icon" href="images/agq_logo.png" type="image/ico">
-
+    <!-- Website Icon -->
+    <link rel="icon" href="images/agq_logo.png" type="image/ico">
 <body>
     <div class="container">
         <div class="row d-flex justify-content-center">
@@ -32,7 +31,7 @@
                     <input type="text" name="email" id="inputs" class="form-control" required>
 
                     <label for="inputs" class="form-label" id="labels">Password</label>
-                    <input type="password" name="password" id="inputs0" class="form-control" required>
+                    <input type="password" name="password" id="inputs0" class="form-control" required> 
 
                     <p class="text-center" id="forgotP"><a href="agq_forgotEmail.php">Forgot Password?</a></p>
                     <div class="d-flex justify-content-center">
@@ -70,95 +69,96 @@
     </script>
 
 </body>
-
 </html>
 
 <?php
-session_start(); // Start the session at the beginning of your script
-require_once "db_agq.php";
-include "agq_mailer.php";
+    session_start(); // Start the session at the beginning of your script
+    require_once "db_agq.php";
+    include "agq_mailer.php";
 
-if (!isset($_SESSION['login_attempts'])) {
-    $_SESSION['login_attempts'] = 1; // Initialize login attempts counter if not set
-}
-if (!isset($_SESSION['last_attempt_time'])) {
-    $_SESSION['last_attempt_time'] = time(); // Initialize last attempt time if not set
-}
+    if (!isset($_SESSION['login_attempts'])) {
+        $_SESSION['login_attempts'] = 1; // Initialize login attempts counter if not set
+    }
+    if (!isset($_SESSION['last_attempt_time'])) {
+        $_SESSION['last_attempt_time'] = time(); // Initialize last attempt time if not set
+    }
 
-// Reset login attempts if 5 minutes have passed since the last attempt
-if (time() - $_SESSION['last_attempt_time'] > 60) {
-    $_SESSION['login_attempts'] = 1;
-}
+    // Reset login attempts if 5 minutes have passed since the last attempt
+    if (time() - $_SESSION['last_attempt_time'] > 10) {
+        $_SESSION['login_attempts'] = 1;
+    }
 
-if ((isset($_POST['email']) && $_POST['email'] != NULL) &&
-    (isset($_POST['password']) && $_POST['password'] != NULL)
-) {
+    if ((isset($_POST['email']) && $_POST['email'] != NULL) && 
+    (isset($_POST['password']) && $_POST['password'] != NULL)) {
 
-    $email = $_POST['email'];
-    $pass = $_POST['password'];
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
 
-    if ($_SESSION['login_attempts'] >= 5) {
-?>
-        <script>
-            Swal.fire({
-                position: "center",
-                icon: "error",
-                title: "Account Locked",
-                text: "Due to numerous failed attempts, you have been locked out for 1 minute.",
-                showConfirmButton: false,
-                timer: 3000
-            });
-            disableInputField(); // Disable input field for 1 minute
-        </script>
-        <?php
-    } else {
-        $loginVerify = "SELECT * FROM tbl_user WHERE Email = '$email' AND Password = '$pass'";
-        $queryVerify = $conn->query($loginVerify);
-
-        if ($queryVerify->num_rows > 0) {
-            $row = $queryVerify->fetch_assoc();
-            $role = $row['Department'];
-            $pword = $row['Password'];
-
-            // Reset login attempts counter on successful login
-            $_SESSION['login_attempts'] = 0;
-
-            if ($role == 'admin' || $role == 'Admin' || $role == 'owner' || $role == 'Owner' && $pword != 'agqLogistics') {
-                header("location: agq_owndash.php");
-            } else if ($role == 'Export Forwarding' || $role == 'Import Forwarding' || $role == 'Export Brokerage' || $role == 'Import Brokerage' && $pword != 'agqLogistics') {
-                header("location: agq_employdash.php");
-            } else {
-                $otp = rand(000000, 999999);
-
-                $otpQuery = "UPDATE tbl_user SET Otp = '$otp' WHERE Email = '$email' AND Password = '$pass'";
-                $conn->query($otpQuery);
-
-                emailVerification($email, $otp);
-            }
-        } else {
-            // Increment login attempts counter on failed login
-            $_SESSION['login_attempts']++;
-            $_SESSION['last_attempt_time'] = time();
-
-        ?>
+        if ($_SESSION['login_attempts'] >= 5) {
+            ?>
             <script>
                 Swal.fire({
                     position: "center",
                     icon: "error",
-                    title: "Invalid Log In",
-                    text: "Account does not Exist or Email and Password does not match.",
+                    title: "Account Locked",
+                    text: "Due to numerous failed attempts, you have been locked out for 1 minute.",
                     showConfirmButton: false,
                     timer: 3000
                 });
+                disableInputField(); // Disable input field for 1 minute
             </script>
-<?php
+            <?php
+        } else {
+            $loginVerify = "SELECT * FROM tbl_user WHERE Email = '$email' AND Password = '$pass'";
+            $queryVerify = $conn->query($loginVerify);
+
+            if ($queryVerify->num_rows > 0) {
+                $row = $queryVerify->fetch_assoc();
+                $role = $row['Department'];
+                $pword = $row['Password'];
+
+                // Reset login attempts counter on successful login
+                $_SESSION['login_attempts'] = 0;
+
+                if ($role == 'admin' || $role == 'Admin' || $role == 'owner' || $role == 'Owner' && $pword != 'agqLogistics') {
+                    header("location:agq_owndash.php");
+                } else if ($role == 'Export Forwarding' || $role == 'Import Forwarding' || $role == 'Export Brokerage' || $role == 'Import Brokerage' && $pword != 'agqLogistics') {
+                    header("location:agq_employdash.php");
+                } else {
+                    $otp = rand(000000,999999);
+                    
+                    $otpQuery = "UPDATE tbl_user SET Otp = '$otp' WHERE Email = '$email' AND Password = '$pass'";
+                    $conn->query($otpQuery);
+
+                    emailVerification($email, $otp);
+
+                }
+
+                session_destroy();
+            }else {
+                // Increment login attempts counter on failed login
+                $_SESSION['login_attempts']++;
+                $_SESSION['last_attempt_time'] = time();
+
+                ?>
+                <script>
+                    Swal.fire({
+                        position: "center",
+                        icon: "error",
+                        title: "Invalid Log In",
+                        text: "Account does not Exist or Email and Password does not match.",
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                </script>
+                <?php
+            }
+
+        
+            $conn->close();    
+        
         }
 
-
-        $conn->close();
     }
-}
-
-session_destroy();
 
 ?>
