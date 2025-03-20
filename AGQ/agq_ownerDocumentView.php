f111057bec6299b035252aaff07e3451bf155939
diyownn <dionderek.almiranez.cics@ust.edu.ph>
2025-03-20 11:20:50 +0800

    correct css pathing

--- AGQ/agq_ownerDocumentView.php
+++ AGQ/agq_ownerDocumentView.php
@@ -21,8 +21,9 @@ $dept = isset($_SESSION['SelectedDepartment']) ? $_SESSION['SelectedDepartment']
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../css/owndocuview.css">
+  <link rel ="stylesheet" href = "../css/owndocu.css">
 </head>
+
 
 <body>
   <div class="container">
