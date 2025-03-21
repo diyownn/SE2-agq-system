980c9752a521983bd2e0b192d21045f499098fec
diyownn <dionderek.almiranez.cics@ust.edu.ph>
2025-03-21 12:12:41 +0800

    minor front-end fixes

--- AGQ/agq_owndash.php
+++ AGQ/agq_owndash.php
@@ -81,7 +81,8 @@ if (!empty($search_query)) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../css/owndash.css">
-    <style>
+    <!-- put this CSS code in owndash.css-->
+    <style>  
         .hamburger-menu {
             display: none;
             flex-direction: column;
